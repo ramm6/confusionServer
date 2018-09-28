47dbb30efeb4d9a0044d29d9c7230a0cb2d169c4
Rammohan <Ram@RAM-LAPPY>
2018-09-28 07:53:34 +0530

    Week 4 - HTTPS service

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ conn.then((db) => {
 
 var app = express();
 
+app.all('*', (req, res, next) => {
+    if (req.secure) {
+        next();
+    } else {
+        // 307 - Target resource is available at different location.
+        res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url)
+    }
+})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
