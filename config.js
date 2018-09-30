c3a1fd72d7e4335f984c18744c25056633258b4f
Rammohan <Ram@RAM-LAPPY>
2018-09-30 11:46:04 +0530

    Passport facebook OAuth2 implementation

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '1234-5678-9012-3456',
-    'mongoUrl': 'mongodb://testuser:password@ds133558.mlab.com:33558/testapp'
+    'mongoUrl': 'mongodb://testuser:password@ds133558.mlab.com:33558/testapp',
+    'facebook':{
+        clientId: '999713126856678',
+        clientSecret: '923539a15a07c435665638c4afde8790'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -74,4 +74,18 @@ router.get('/logout', cors.cors, (req, res, next) => {
     }
 });
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+    if (req.user) {
+        var token = authenticate.getToken({ _id: req.user._id });
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            success: true,
+            token: token,
+            status: "You had successfully loggedin"
+        });
+
+    }
+})
+
 module.exports = router;
